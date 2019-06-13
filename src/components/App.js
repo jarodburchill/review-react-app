55cac976f4b601d27b410baa17225d6a52c9ac8f
BURCHILL <jarodburchill@gmail.com>
2019-06-13 12:03:14 -0400

    Added tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 const App = () => {
+  const [value, setValue] = useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
   return (
     <div>
+      <Tabs
+        value={value} 
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Add Review" />
+        <Tab label="List Reviews" />
+        <Tab label="Settings" />
+      </Tabs>
+      {value === 0 && <div>Item One</div>}
+      {value === 1 && <div>Item Two</div>}
+      {value === 2 && <div>Item Three</div>}
     </div>
   );
 }
