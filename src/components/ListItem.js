eb02dcc6e9da6186074942490851847dbaa50515
BURCHILL <jarodburchill@gmail.com>
2019-06-14 12:34:16 -0400

    Display plain-text data

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,6 +4,11 @@ const ListItem = (props) => {
 
   return (
     <div>
+      {props.reviewObject.businessName}
+      {props.reviewObject.email}
+      {props.reviewObject.rating}
+      {props.reviewObject.comments}
+      {props.reviewObject.date}
     </div>
   );
 }
--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,14 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import ListItem from './ListItem';
 
 const ListPage = (props) => {
+  const [reviewObjects, setReviewObjects] = useState([]);
 
   useEffect(() => {
-    
+    if (localStorage.getItem("reviews") !== null) {
+      const reviews = localStorage.getItem("reviews");
+      setReviewObjects(reviews.split(":::"));
+    }
   }, []);
 
   return (
     <div>
-      2
+      {reviewObjects.map((item, index) => (
+        <ListItem key={index} index={index} reviewObject={JSON.parse(item)} />
+      ))}
     </div>
   );
 }
