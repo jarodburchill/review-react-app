d1b9de8863c29860cb87b69c6a83b88025cb64cd
BURCHILL <jarodburchill@gmail.com>
2019-06-17 15:33:05 -0400

    Updates

---
+++ precache-manifest.320a5526aa0e496079f40ec78535abc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f924814108b64f44048fc204144f2e6",
+    "url": "/ReviewReactApp/index.html"
+  },
+  {
+    "revision": "baf8d79be17f6b9c263c",
+    "url": "/ReviewReactApp/static/css/main.98dfaaa0.chunk.css"
+  },
+  {
+    "revision": "729adc283d879f07a05f",
+    "url": "/ReviewReactApp/static/js/2.a0066b3b.chunk.js"
+  },
+  {
+    "revision": "baf8d79be17f6b9c263c",
+    "url": "/ReviewReactApp/static/js/main.70cb142d.chunk.js"
+  },
+  {
+    "revision": "6b77f700d18115235713",
+    "url": "/ReviewReactApp/static/js/runtime~main.47c13d3c.js"
+  }
+]);
